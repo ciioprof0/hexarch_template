11d269a7228512c59dd86ec185ef88c10ee592be
ciioprof0 <116185051+ciioprof0@users.noreply.github.com>
2024-08-25 07:17:27 -0700

    Added Docker files

--- src/config.py
+++ src/config.py
@@ -6,3 +6,8 @@
 
 import os
 
+class Config:
+    """Set Flask configuration vars from .env file."""
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///mydatabase.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'mysecret')
---
+++ src/entrypoints/api/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# src/entrypoints/api/app.py
+
+""" Entry point for the Flask application. """
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def api_home():
+    return jsonify({"message": "Hello, HexArch!"})
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
