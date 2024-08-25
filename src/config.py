c98d3b49c82622d3be9ecd5ab765c64aef4c1157
ciioprof0 <116185051+ciioprof0@users.noreply.github.com>
2024-08-25 06:59:36 -0700

    Added /src

---
+++ src/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#src/config.py
+
+"""This module contains the configuration variables for the application."""
+
+import os
+
---
+++ src/ml_models/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#src/ml_models/__init__.py
